06440cd841c8353b4295fb07e2446a9ad8e7d6c0
hayrettinm <hayrettinmavis@gmail.com>
2021-01-13 09:22:37 +0300

    initial commit

---
+++ src/main/java/com/hayrettinm/carstorecarsrv/CarStoreCarSrvApplication.java
@@ -0,0 +1,13 @@
+package com.hayrettinm.carstorecarsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStoreCarSrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarStoreCarSrvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hayrettinm/carstorecarsrv/CarStoreCarSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayrettinm.carstorecarsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStoreCarSrvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
